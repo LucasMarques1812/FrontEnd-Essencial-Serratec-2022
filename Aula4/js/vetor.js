1663e4275e4da2625f43b337d3ccce46bdcb64ab
Lucas Marques <Colten610@gmail.com>
2022-09-23 04:53:25 -0300

    mod na aula3. Aula 4 criada
    modificações na aula3 e aula4 criada hoje

---
+++ Aula4/js/vetor.js
@@ -0,0 +1,22 @@
+var frutas = new Array();
+var cores=[];
+var cursos = ["JAVA","HTML","C++","Ruby"];
+
+cores.push("Vermelho");
+cores.push("Azul");
+cores.push("Branco");
+cores.push(1000);
+
+frutas.push("Goiaba");
+frutas[1]= "Laranja";
+
+//console.log(cursos[0]);
+for (let i = 0; i < array.length; i++) {
+  console.log(cursos[i]);  
+}
+
+console.log("---------------------------------")
+
+cursos.forEach(c => {
+    console.log(c);
+});
